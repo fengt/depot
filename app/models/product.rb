9f4d5180b9ec59d95a510f236336239b3f10bb4e
fhalcyon <fhalcyon@gmail.com>
2016-09-05 23:46:14 +0800

    Cart Creation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,7 @@
 class Product < ApplicationRecord
 	default_scope { order('title') }
+	has_many :line_items
+	before_destroy :ensure
 
 	validates :title, :description, :image_url, presence: true
 	validates :price, numericality: {greater_than_or_equal_to: 0.01}
@@ -9,4 +11,16 @@ class Product < ApplicationRecord
 		with: /\A*\.(gif|jpg|png)$\z/i,
 		message: 'must be a URL for GIF, JPG or PNG image.'
 	}
+
+	private
+
+	# ensure that there are no line items referencing this product
+	def ensure_not_referenced_by_any_line_item
+		if line_items.empty?
+			return true
+		else
+			errors.add(:base, 'Line Item present')
+			return false
+		end
+	end
 end
