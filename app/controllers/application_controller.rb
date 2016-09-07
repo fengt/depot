bae4d3404c6123ae1e90c35aa8c57b8ae29a6c31
fhalcyon <fhalcyon@gmail.com>
2016-09-07 18:26:55 +0800

    A Smarter Cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
   private
 
@@ -10,4 +11,10 @@ class ApplicationController < ActionController::Base
   		session[:cart_id] = cart.id
   		cart
   	end
+
+
+  	def record_not_found
+  		render plain: "404 Not Found", status: 404
+  	end
+
 end
