14324b3a2477756f3ec3eaec768b0f3a573a40d3
fhalcyon <fhalcyon@gmail.com>
2016-10-18 21:05:03 +0800

    AJAX

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,15 +4,6 @@ class ApplicationController < ActionController::Base
 
   private
 
-  	def current_cart
-  		Cart.find(session[:cart_id])
-  	rescue ActiveRecord::RecordNotFound
-  		cart = Cart.create
-  		session[:cart_id] = cart.id
-  		cart
-  	end
-
-
   	def record_not_found
   		render plain: "404 Not Found", status: 404
   	end
