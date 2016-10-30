f89ac0bc01470c404cc4dd7fc42274de0de57b70
fhalcyon <fhalcyon@gmail.com>
2016-10-30 23:38:49 +0800

    Loggin In

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :authorize
   protect_from_forgery with: :exception
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
@@ -7,5 +8,20 @@ class ApplicationController < ActionController::Base
   	def record_not_found
   		render plain: "404 Not Found", status: 404
   	end
+
+  protected
+    
+    def authorize
+      if request.format == Mime[:html]
+        unless User.find_by(id: session[:user_id])
+          redirect_to login_url, notice: "Please log in"
+        end
+      else
+        authenticate_or_request_with_http_basic do |username, password|
+          user = User.find_by(name: username)
+          user && user.authenticate(password)
+        end
+      end
+    end
 
 end
