cc031e2f1ad55e7c8077cf8da10daecdb242dd71
fhalcyon <fhalcyon@gmail.com>
2016-11-05 13:37:26 +0800

    Internationalization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :set_i18n_locale_from_params
   before_action :authorize
   protect_from_forgery with: :exception
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
@@ -10,6 +11,17 @@ class ApplicationController < ActionController::Base
   	end
 
   protected
+    
+    def set_i18n_locale_from_params
+      if params[:locale]
+        if I18n.available_locales.map(&:to_s).include?(params[:locale])
+          I18n.locale = params[:locale]
+        else
+          flash.now[:notice] = "#{params[:locale]} translation not available"
+          logger.error flash.now[:notice]
+        end
+      end
+    end
     
     def authorize
       if request.format == Mime[:html]
